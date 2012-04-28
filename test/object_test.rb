1bb55e26d2b41ceffc35c2e0c8cf6e1448eabed0
Nick Griffiths <nick@mediasp.com>
2012-04-28 22:18:02 +0100

    test/object_test.rb passes

--- test/object_test.rb
+++ test/object_test.rb
@@ -1,14 +1,13 @@
 require 'test/helpers'
-require 'test/api/resource/helpers'
 
 describe "An Resource::Object resource class" do
 
   class TestObjectResourceClass < Resource::Object
-    register_type do
+    register_type TEST_REGISTRY do
       property :foo, :integer
       property :bar, sequence(:string)
     end
-    
+
     def initialize(uri, app_context, properties)
       super(uri, app_context)
       @foo = properties[:foo]
@@ -17,19 +16,20 @@ describe "An Resource::Object resource class" do
   end
 
   include ResourceTestHelpers
-  
+  include WirerHelpers
+
   def setup
-    @msp_app = MSP.new_application('resource2' => {:type_index_uri => '/types'}) do |app|
-      app.add_instance({TestObjectResourceClass => 'test-object-resource-class'}, :features => [:classes_exposed_as_type_resources])
+    @ctr = new_container do |ctr|
+      ctr.add_instance({TestObjectResourceClass => 'test-object-resource-class'}, :features => [:classes_exposed_as_type_resources])
     end
-        
-    @resource = TestObjectResourceClass.new('/under_test', @msp_app.resource_application_context, :foo => 123, :bar => ['x','y','z'])
-    
+
+    @resource = TestObjectResourceClass.new('/under_test', @ctr.resource_application_context, :foo => 123, :bar => ['x','y','z'])
+
     self.root_resource = Class.new {include Doze::Router}.new
-    root_resource.add_route('/types', :to => @msp_app.type_index)
-    root_resource.add_route('/under_test', :to => @resource)    
+    root_resource.add_route('/types', :to => @ctr.type_index)
+    root_resource.add_route('/under_test', :to => @resource)
   end
-  
+
   it "should serialize itself according to its registered type, and include its uri in the serialization" do
     get '/under_test'
     assert_equal({
@@ -40,7 +40,7 @@ describe "An Resource::Object resource class" do
       'uri'       => '/under_test'
     }, last_response.json)
   end
-  
+
   it "should make properties accessible via subresources" do
     get '/under_test/property/foo'
     assert last_response.ok?
